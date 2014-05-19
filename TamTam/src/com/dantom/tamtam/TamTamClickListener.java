9b9fb403d8cb5516bb0e97d508cf293deb66b82d
Dan Evgi <danevgi@gmail.com>
2014-05-19 19:11:09 +0300

    unused

--- TamTam/src/com/dantom/tamtam/TamTamClickListener.java
+++ TamTam/src/com/dantom/tamtam/TamTamClickListener.java
@@ -1,7 +1,5 @@
 package com.dantom.tamtam;
 
-import java.util.Random;
-
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
@@ -11,7 +9,6 @@ import android.view.View.OnClickListener;
 public class TamTamClickListener implements OnClickListener {
 	
 	private int sound;
-	private Random r = new Random(System.currentTimeMillis());
 	private Context context;
 	
 	public TamTamClickListener (int sound, Context context){
